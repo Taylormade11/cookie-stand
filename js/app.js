a10e0bd12659735f2fc9c328030b291ddef30e26
Taylor Ellis <Taylorellis11@gmail.com>
2018-04-09 16:54:05 -0700

    Figured out the logic, moving on to the rest of the questions now.

--- js/app.js
+++ js/app.js
@@ -1,31 +1,72 @@
 'use strict';
 
+
+console.log('hi');
+
+var hrsOfOperation = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', 'Total'];
+
 var firstAndPike = {
-  minCustPerDay: 23,
-  maxCustPerDay: 65,
+  minCustPerHr: 23,
+  maxCustPerHr: 65,
   avgSalePerCust: 6.3,
+  cookiesSoldEachHr: [],
+  calcCookiesPerHr: function() {
+    for (var i = 0; i < hrsOfOperation.length - 1; i++) {
+      var numOfCustsPerHr = Math.round(Math.random() * (65 - 23) + 23);
+      var ulItemElement = document.getElementById('firstAndPike');
+      this.cookiesSoldEachHr[i] = Math.round(numOfCustsPerHr * this.avgSalePerCust);
+      var listItemElement = document.createElement('li');
+      listItemElement.textContent = hrsOfOperation[i] + ': ' + this.cookiesSoldEachHr[i];
+      ulItemElement.appendChild(listItemElement);
+    }
+  }
 };
+
+firstAndPike.calcCookiesPerHr();
 
 var seaTacAirport = {
-  minCustPerDay: 3,
-  maxCustPerDay: 24,
+  minCustPerHr: 3,
+  maxCustPerHr: 24,
   avgSalePerCust: 1.2,
+  cookiesSoldEachHr: [],
+  calcCookiesPerHr: function() {
+    for (var i = 0; i < hrsOfOperation.length - 1; i++) {
+      var numOfCustsPerHr = Math.round(Math.random() * (24 - 3) + 3);
+      var ulItemElement = document.getElementById('seaTacAirport');
+      this.cookiesSoldEachHr[i] = Math.round(numOfCustsPerHr * this.avgSalePerCust);
+      var listItemElement = document.createElement('li');
+      listItemElement.textContent = hrsOfOperation[i] + ': ' + this.cookiesSoldEachHr[i];
+      ulItemElement.appendChild(listItemElement);
+    }
+  }
 };
 
-var seattleCenter = {
-  minCustPerDay: 11,
-  maxCustPerDay: 38,
-  avgSalePerCust: 3.7
-};
+seaTacAirport.calcCookiesPerHr();
 
-var capitolHill = {
-  minCustPerDay: 20,
-  maxCustPerDay: 38,
-  avgSalePerCust: 2.3
-};
 
-var alki = {
-  minCustPerDay: 2,
-  maxCustPerDay: 16,
-  avgSalePerCust: 4.6
-}
+// var seattleCenter = {
+//   minCustPerHr: 11,
+//   maxCustPerHr: 38,
+//   avgSalePerCust: 3.7
+//   cookiesSoldEachHr: [],
+//   avgCustsPerHr: Math.random() * (38 - 11) + 11,
+//   avgCookiesPerHour: avgCustsPerHr * avgSalePerCust,
+// };
+
+// var capitolHill = {
+//   minCustPerHr: 20,
+//   maxCustPerHr: 38,
+//   avgSalePerCust: 2.3
+//   cookiesSoldEachHr: [],
+//   avgCustsPerHr: Math.random() * (38 - 20) + 20,
+//   avgCookiesPerHour: avgCustsPerHr * avgSalePerCust,
+// };
+
+// var alki = {
+//   minCustPerHr: 2,
+//   maxCustPerHr: 16,
+//   avgSalePerCust: 4.6
+//   cookiesSoldEachHr: [],
+//   avgCustsPerHr: Math.random() * (16 - 2) + 2,
+//   avgCookiesPerHour: avgCustsPerHr * avgSalePerCust,
+// }
