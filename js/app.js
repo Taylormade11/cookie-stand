339a546b9b377c2f076fcf71578d1af15a655216
Taylor Ellis <Taylorellis11@gmail.com>
2018-04-09 14:00:19 -0700

    Store objects base set up and sales.html added

---
+++ js/app.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var firstAndPike = {
+  minCustPerDay: 23,
+  maxCustPerDay: 65,
+  avgSalePerCust: 6.3,
+};
+
+var seaTacAirport = {
+  minCustPerDay: 3,
+  maxCustPerDay: 24,
+  avgSalePerCust: 1.2,
+};
+
+var seattleCenter = {
+  minCustPerDay: 11,
+  maxCustPerDay: 38,
+  avgSalePerCust: 3.7
+};
+
+var capitolHill = {
+  minCustPerDay: 20,
+  maxCustPerDay: 38,
+  avgSalePerCust: 2.3
+};
+
+var alki = {
+  minCustPerDay: 2,
+  maxCustPerDay: 16,
+  avgSalePerCust: 4.6
+}
