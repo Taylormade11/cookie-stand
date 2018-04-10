ef643fe48d5068387c99cc36f3f3f3bfbcd6c3dd
Taylor Ellis <Taylorellis11@gmail.com>
2018-04-09 18:41:59 -0700

    Ready to style, lists are functional with totals.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+console.log('Don\'t forget to style!');
+
 var hrsOfOperation = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', 'Total'];
 
 var firstAndPike = {
