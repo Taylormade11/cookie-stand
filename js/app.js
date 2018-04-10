5734196d75a37474495bbfec2e189c669965ac97
Taylor Ellis <Taylorellis11@gmail.com>
2018-04-09 18:19:35 -0700

    Finally figured out how to make another method that would keep track of the TotalCookies that was being incriminted

--- js/app.js
+++ js/app.js
@@ -1,8 +1,5 @@
 'use strict';
 
-
-console.log('hi');
-
 var hrsOfOperation = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', 'Total'];
 
 var firstAndPike = {
@@ -10,63 +7,96 @@ var firstAndPike = {
   maxCustPerHr: 65,
   avgSalePerCust: 6.3,
   cookiesSoldEachHr: [],
+  totalCookies: 0,
   calcCookiesPerHr: function() {
-    for (var i = 0; i < hrsOfOperation.length - 1; i++) {
+    for (var i = 0; i < hrsOfOperation.length -1; i++) {
       var numOfCustsPerHr = Math.round(Math.random() * (65 - 23) + 23);
       var ulItemElement = document.getElementById('firstAndPike');
       this.cookiesSoldEachHr[i] = Math.round(numOfCustsPerHr * this.avgSalePerCust);
+      this.totalCookies += this.cookiesSoldEachHr[i];
       var listItemElement = document.createElement('li');
       listItemElement.textContent = hrsOfOperation[i] + ': ' + this.cookiesSoldEachHr[i];
       ulItemElement.appendChild(listItemElement);
     }
+  },
+  calcTotalCookies: function() {
+    var ulItemElement = document.getElementById('firstAndPike');
+    var listItemElement = document.createElement('li');
+    listItemElement.textContent = hrsOfOperation[15] + ': ' + this.totalCookies;
+    ulItemElement.appendChild(listItemElement);
   }
 };
 
-firstAndPike.calcCookiesPerHr();
 
 var seaTacAirport = {
   minCustPerHr: 3,
   maxCustPerHr: 24,
   avgSalePerCust: 1.2,
   cookiesSoldEachHr: [],
+  totalCookies: 0,
   calcCookiesPerHr: function() {
     for (var i = 0; i < hrsOfOperation.length - 1; i++) {
       var numOfCustsPerHr = Math.round(Math.random() * (24 - 3) + 3);
       var ulItemElement = document.getElementById('seaTacAirport');
       this.cookiesSoldEachHr[i] = Math.round(numOfCustsPerHr * this.avgSalePerCust);
+      this.totalCookies += this.cookiesSoldEachHr[i];
       var listItemElement = document.createElement('li');
       listItemElement.textContent = hrsOfOperation[i] + ': ' + this.cookiesSoldEachHr[i];
       ulItemElement.appendChild(listItemElement);
     }
+  },
+  calcTotalCookies: function() {
+    var ulItemElement = document.getElementById('seaTacAirport');
+    var listItemElement = document.createElement('li');
+    listItemElement.textContent = hrsOfOperation[15] + ': ' + this.totalCookies;
+    ulItemElement.appendChild(listItemElement);
   }
 };
 
-seaTacAirport.calcCookiesPerHr();
-
-
-// var seattleCenter = {
-//   minCustPerHr: 11,
-//   maxCustPerHr: 38,
-//   avgSalePerCust: 3.7
-//   cookiesSoldEachHr: [],
-//   avgCustsPerHr: Math.random() * (38 - 11) + 11,
-//   avgCookiesPerHour: avgCustsPerHr * avgSalePerCust,
-// };
+var seattleCenter = {
+  minCustPerHr: 11,
+  maxCustPerHr: 38,
+  avgSalePerCust: 3.7,
+  cookiesSoldEachHr: [],
+  totalCookies: 0,
+  calcCookiesPerHr: function() {
+    for (var i = 0; i < hrsOfOperation.length - 1; i++) {
+      var numOfCustsPerHr = Math.round(Math.random() * (38 - 11) + 11);
+      var ulItemElement = document.getElementById('seattleCenter');
+      this.cookiesSoldEachHr[i] = Math.round(numOfCustsPerHr * this.avgSalePerCust);
+      this.totalCookies += this.cookiesSoldEachHr[i];
+      var listItemElement = document.createElement('li');
+      listItemElement.textContent = hrsOfOperation[i] + ': ' + this.cookiesSoldEachHr[i];
+      ulItemElement.appendChild(listItemElement);
+    }
+  },
+  calcTotalCookies: function() {
+    var ulItemElement = document.getElementById('seattleCenter');
+    var listItemElement = document.createElement('li');
+    listItemElement.textContent = hrsOfOperation[15] + ': ' + this.totalCookies;
+    ulItemElement.appendChild(listItemElement);
+  }
+};
 
 // var capitolHill = {
 //   minCustPerHr: 20,
 //   maxCustPerHr: 38,
 //   avgSalePerCust: 2.3
 //   cookiesSoldEachHr: [],
-//   avgCustsPerHr: Math.random() * (38 - 20) + 20,
-//   avgCookiesPerHour: avgCustsPerHr * avgSalePerCust,
-// };
+
+// }
 
 // var alki = {
 //   minCustPerHr: 2,
 //   maxCustPerHr: 16,
 //   avgSalePerCust: 4.6
 //   cookiesSoldEachHr: [],
-//   avgCustsPerHr: Math.random() * (16 - 2) + 2,
-//   avgCookiesPerHour: avgCustsPerHr * avgSalePerCust,
+
 // }
+
+firstAndPike.calcCookiesPerHr();
+firstAndPike.calcTotalCookies();
+seaTacAirport.calcCookiesPerHr();
+seaTacAirport.calcTotalCookies();
+seattleCenter.calcCookiesPerHr();
+seattleCenter.calcTotalCookies();
