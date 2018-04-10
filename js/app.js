652e7fe3babc96d8083dc9273340d9f8f44047b2
Taylor Ellis <Taylorellis11@gmail.com>
2018-04-10 15:42:51 -0700

    Styling everything, table is fully functional and colored. Working on front facing web page and anything I missed in the objectives of lab.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,6 @@
 'use strict';
 
-console.log('Don\'t forget to style!');
-
 var hrsOfOperation = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', 'Total'];
-
-// var listOfStores = ['firstAndPike', 'seaTacAirport', 'seattleCenter', 'capitolHill', 'alki'];
 
 function CreateStore(storeName, minCustPerHr, maxCustPerHr, avgSalePerCust) {
   this.cookiesSoldEachHr = [];
@@ -15,33 +11,61 @@ function CreateStore(storeName, minCustPerHr, maxCustPerHr, avgSalePerCust) {
   this.avgSalePerCust = avgSalePerCust;
 }
 
+CreateStore.prototype.renderHeader = function() {
+  var tableElement = document.getElementById('table');
+  var tableRow = document.createElement('tr');
+  tableRow.setAttribute('class', 'tableHead');
+  tableElement.appendChild(tableRow);
+  var headTime = document.createElement('td');
+  headTime.textContent = 'Time';
+  tableRow.appendChild(headTime);
+  for (var i in hrsOfOperation) {
+    tableElement = document.getElementById('table');
+    headTime = document.createElement('td');
+    headTime.textContent = hrsOfOperation[i];
+    tableRow.appendChild(headTime);
+  }
+};
+
 CreateStore.prototype.cookiesPerHr = function() {
+  var tableRow = document.createElement('tr');
+  tableElement = document.getElementById('table');
+  var strName = document.createElement('td');
+  strName.textContent = this.storeName;
+  strName.setAttribute('class', 'stores');
+  tableRow.appendChild(strName);
+  tableRow.setAttribute('id', this.storeName);
+
   for (var i = 0; i < hrsOfOperation.length -1; i++) {
     var numOfCustsPerHr = Math.round(Math.random() * (this.maxCustPerHr - this.minCustPerHr) + this.minCustPerHr);
-    var ulItemElement = document.getElementById(this.storeName);
+    var tableElement = document.getElementById('table');
     var randomCookies = (Math.round(numOfCustsPerHr * this.avgSalePerCust));
-    console.log(this.totalCookies);
     this.cookiesSoldEachHr.push(randomCookies);
     this.totalCookies += randomCookies;
-    var listItemElement = document.createElement('li');
-    listItemElement.textContent = hrsOfOperation[i] + ': ' + randomCookies;
-    ulItemElement.appendChild(listItemElement);
+    tableRow.textcontent = hrsOfOperation[i];
+    tableElement.appendChild(tableRow);
+    var tableData = document.createElement('td');
+    tableData.textContent = randomCookies;
+    tableRow.appendChild(tableData);
   }
 };
 
 CreateStore.prototype.getTotal = function() {
-  var ulItemElement = document.getElementById(this.storeName);
-  var listItemElement = document.createElement('li');
-  listItemElement.textContent = hrsOfOperation[15] + ': ' + this.totalCookies;
-  ulItemElement.appendChild(listItemElement);
+  var tableElement = document.getElementById('table');
+  var tableRow = document.getElementById(this.storeName);
+  tableElement.appendChild(tableRow);
+  var grandTotal = document.createElement('td');
+  grandTotal.textContent = this.totalCookies;
+  tableRow.appendChild(grandTotal);
 };
 
-var firstAndPike = new CreateStore('firstAndPike', 23, 65, 6.3);
-var seaTacAirport = new CreateStore('seaTacAirport', 3, 24, 1.2);
-var seattleCenter = new CreateStore('seattleCenter', 11, 38, 3.7);
-var capitolHill = new CreateStore('capitolHill', 20, 38, 2.3);
-var alki = new CreateStore('alki', 2, 16, 4.6);
+var firstAndPike = new CreateStore('1st & Pike', 23, 65, 6.3);
+var seaTacAirport = new CreateStore('SeaTac Airport', 3, 24, 1.2);
+var seattleCenter = new CreateStore('Seattle Center', 11, 38, 3.7);
+var capitolHill = new CreateStore('Capitol Hill', 20, 38, 2.3);
+var alki = new CreateStore('Alki', 2, 16, 4.6);
 
+firstAndPike.renderHeader();
 firstAndPike.cookiesPerHr();
 firstAndPike.getTotal();
 seaTacAirport.cookiesPerHr();
