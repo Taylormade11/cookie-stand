37f983f77095ca718674b8f979803036c8f8306e
Taylor Ellis <Taylorellis11@gmail.com>
2018-04-12 00:23:45 -0700

    Trying to accomplish stretch goal, but my totals wouldn't update when adding a new store. The array would update the new 15 values, but the for loops wouldn't account for this. Going to try again with a constructor instead.

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@
 
 var hrsOfOperation = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', 'Daily Store Total'];
 
+var hourlyAllCookies = [];
+
 var submitButton = document.getElementById('submit');
 submitButton.addEventListener('click', function() {
 });
@@ -11,10 +13,9 @@ addStoreElement.addEventListener('submit', handleNewStore);
 
 function handleNewStore(event) {
   event.preventDefault();
-  alert('Thank you for your interest in our Franchise! We will crunch the numbers and see if a store at this location makes sense.');
+  alert('Thank you for your interest in our Franchise! We will crunch the numbers and see if a store at this location will be profitable!');
   var submitButton = event.target;
   var newStore = new CreateStore(submitButton.storeLocation.value, submitButton.minCust.value, submitButton.maxCust.value, submitButton.avgSale.value);
-  console.log(newStore);
   newStore.cookiesPerHr();
   newStore.getTotal();
 }
@@ -58,6 +59,7 @@ CreateStore.prototype.cookiesPerHr = function() {
     var tableElement = document.getElementById('table');
     var randomCookies = (Math.round(numOfCustsPerHr * this.avgSalePerCust));
     this.cookiesSoldEachHr.push(randomCookies);
+    hourlyAllCookies.push(randomCookies);
     this.totalCookies += randomCookies;
     tableRow.textcontent = hrsOfOperation[i];
     tableElement.appendChild(tableRow);
@@ -93,3 +95,54 @@ capitolHill.cookiesPerHr();
 capitolHill.getTotal();
 alki.cookiesPerHr();
 alki.getTotal();
+
+
+// Gets proper totals, but doesn't work when we add a new store. The array updates to a new length of 90, but the for loops don't account for the extra 15 items in the array for some reason. Need to try accomplishing this again with a constructor instead.
+
+// var sixAMTotal = 0;var sevenAMTotal =0;var eightAMTotal =0;var nineAMTotal =0;var tenAMTotal =0;var elevenAMTotal =0;var twelvePMTotal =0;var onePMTotal =0;var twoPMTotal =0;var threePMTotal =0;var fourPMTotal =0;var fivePMTotal =0;var sixPMTotal =0;var sevenPMTotal =0;var eightPMTotal =0;
+
+// for (var i = 0; i < hourlyAllCookies.length; i += 15) {
+//   sixAMTotal += hourlyAllCookies[i];
+// }
+// for (var i = 1; i < hourlyAllCookies.length; i += 15) {
+//   sevenAMTotal += hourlyAllCookies[i];
+// }
+// for (var i = 2; i < hourlyAllCookies.length; i += 15) {
+//   eightAMTotal += hourlyAllCookies[i];
+// }
+// for (var i = 3; i < hourlyAllCookies.length; i += 15) {
+//   nineAMTotal += hourlyAllCookies[i];
+// }
+// for (var i = 4; i < hourlyAllCookies.length; i += 15) {
+//   tenAMTotal += hourlyAllCookies[i];
+// }
+// for (var i = 5; i < hourlyAllCookies.length; i += 15) {
+//   elevenAMTotal += hourlyAllCookies[i];
+// }
+// for (var i = 6; i < hourlyAllCookies.length; i += 15) {
+//   twelvePMTotal += hourlyAllCookies[i];
+// }
+// for (var i = 7; i < hourlyAllCookies.length; i += 15) {
+//   onePMTotal += hourlyAllCookies[i];
+// }
+// for (var i = 8; i < hourlyAllCookies.length; i += 15) {
+//   twoPMTotal += hourlyAllCookies[i];
+// }
+// for (var i = 9; i < hourlyAllCookies.length; i += 15) {
+//   threePMTotal += hourlyAllCookies[i];
+// }
+// for (var i = 10; i < hourlyAllCookies.length; i += 15) {
+//   fourPMTotal += hourlyAllCookies[i];
+// }
+// for (var i = 11; i < hourlyAllCookies.length; i += 15) {
+//   fivePMTotal += hourlyAllCookies[i];
+// }
+// for (var i = 12; i < hourlyAllCookies.length; i += 15) {
+//   sixPMTotal += hourlyAllCookies[i];
+// }
+// for (var i = 13; i < hourlyAllCookies.length; i += 15) {
+//   sevenPMTotal += hourlyAllCookies[i];
+// }
+// for (var i = 14; i < hourlyAllCookies.length; i += 15) {
+//   eightPMTotal += hourlyAllCookies[i];
+// }
